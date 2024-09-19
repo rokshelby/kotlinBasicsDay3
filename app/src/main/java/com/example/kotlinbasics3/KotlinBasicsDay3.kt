59ced3c12cd2c1c1ae247cc238b6712a1a64a6d0
rokshelby <rokshelby@gmail.com>
2024-09-18 22:23:00 -0500

    add function getNumber function -> get a string, then return the int. divide function

--- app/src/main/java/com/example/kotlinbasics3/KotlinBasicsDay3.kt
+++ app/src/main/java/com/example/kotlinbasics3/KotlinBasicsDay3.kt
@@ -4,12 +4,18 @@ package com.example.kotlinbasics3
         val numInt1 = getNumber()
         val numInt2 = getNumber()
         val result = add(numInt1,numInt2)
+        val resultDivide = divide(numInt1,numInt2)
 
         println("The result of $numInt1 + $numInt2 is: $result")
+        println("The result of $numInt1 // $numInt2 is: $resultDivide")
     }
 
     fun add(num1 : Int, num2: Int):Int{
         return num1 + num2
+    }
+
+    fun divide(num1: Int, num2: Int):Double{
+        return num1 / num2.toDouble()
     }
 
     fun getNumber():Int{
